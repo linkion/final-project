4b8cfd805df0ce2b92fa107601f2dfdea550f310
Patrick Orave <42103411+linkion@users.noreply.github.com>
2025-05-05 21:40:24 -0500

    finish?

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ pkgs.git pkgs.pandoc pkgs.texliveMedium ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
